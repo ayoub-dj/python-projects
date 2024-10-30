1ce3d7d9a1f3c239e5d8fb31baf6093d357b1279
service-01 <service@aol.com>
2024-10-29 21:34:44 -0700

    x

---
+++ sftp-client-1/conf.py
@@ -0,0 +1,4 @@
+command = '/home/django_env/bin/gunicorn'
+pythonpath = '/home/myproject'
+bind = ':8000'
+workers = 3
--- sftp-client-1/sftp.py
+++ sftp-client-1/sftp.py
@@ -1,10 +1,12 @@
 import paramiko
 from decouple import config
+import os
 
 HOST = config('SFTP_HOST', cast=str)
 PORT = config('SFTP_PORT', cast=int)
 USERNAME = config('SFTP_USERNAME', cast=str)
 PASSWORD = config('SFTP_PASSWORD', cast=str)
+
 
 
 ssh = paramiko.SSHClient()
@@ -15,10 +17,21 @@ try:
     print("Connected to the SFTP server.")
     
     sftp = ssh.open_sftp()
-    sftp.chdir('/')
-    
+
+    path = '/home/conf/'
+    sftp.chdir(path)
+
     files = sftp.listdir()
     print(files)
+
+    remote_file = 'gunicorn_config.py'
+    local_path = rf'{os.getcwd()}\sftp-client-1\conf.py'
+
+    sftp.get(remotepath=remote_file, localpath=local_path)
+
+    print('#' * 33)
+    print(f'Downloaded {remote_file} to {local_path}')
+
 
 except Exception as e:
     print(f"An error occurred: {e}")
